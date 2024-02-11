a9c3f706cddaa9c997788f120964af34737698e7
azatich <azatabdirashituly@gmail.com>
2024-02-11 19:45:29 +0600

    changed line of routes

--- routes/router.js
+++ routes/router.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { controller } = require('../controller/controllers')
-const { adminController } = require('../controller/adminController')
+const { controller } = require('../controller/controllers');
+const { adminController } = require('../controller/adminController');
 
+// Routes for general users
+router.get('/weatherApp', controller.getWeatherPage);
+router.post('/weather', controller.getWeatherData);
+router.get('/', controller.getRegistrationPage);
+router.post('/getDataFromReg', controller.register);
+router.get('/login', controller.getLoginPage);
+router.post('/getDataFromLog', controller.login);
+router.get('/main', controller.getMainPage);
+router.post('/generateImage', controller.generateImage);
 
-router.get('/', controller.getRegistrationPage)
-router.post('/getDataFromReg', controller.register)
-router.get('/login', controller.getLoginPage)
-router.post('/getDataFromLog', controller.login)
-router.get('/main', controller.getMainPage)
-router.get('/adminPage', controller.getAdminPage)
-router.post('/main', controller.generateImage)
-
-//weather app routes
-router.get('/weatherApp', controller.getWeatherPage)
-router.post('/weather', controller.getWeatherData)
-
-router.post('/addUser', adminController.addUser)
-router.post('/deleteUser', adminController.deleteUser)
-router.post('/editUser', adminController.editUser)
-router.get('/deletedUsers', adminController.getDeletedUsers)
-router.get('/lastActive', adminController.getlastActive)
+// Routes for admin
+router.get('/adminPage', controller.getAdminPage);
+router.post('/addUser', adminController.addUser);
+router.post('/deleteUser', adminController.deleteUser);
+router.post('/editUser', adminController.editUser);
+router.get('/deletedUsers', adminController.getDeletedUsers);
+router.get('/lastActive', adminController.getlastActive);
 
 module.exports = router;
